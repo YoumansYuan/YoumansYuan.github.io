bd403525e94dcf86424038f12594d7f46cfa6d53
YoumansYuan <15563982003ysj@gmail.com>
2020-12-14 14:08:56 +0800

    homepagefinish

---
+++ Youmans.js
@@ -0,0 +1,19 @@
+
+function greeting(){
+var x = prompt("hello, what's your gender?");
+  if(x == "male"){
+    alert("hello sir, welcome");
+  }
+  else if(x == "female"){
+    alert("hello lady, welcome!");
+  }
+  else{
+    alert("please indicate your gender by male or female");
+      }
+}
+
+
+function date(){
+var x = new Date();
+  alert("Today is "+x+" Greetings!");
+}
